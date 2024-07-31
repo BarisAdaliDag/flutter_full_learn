b07509379dbdc031bae85c9f81f7bf874100542e
barisAdaliDag <barisadali.dag@hotmail.com>
2024-07-31 23:09:53 +0300

    first commit

---
+++ lib/101/image_learn.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class ImageLaern extends StatelessWidget {
+  const ImageLaern({super.key});
+  final String _imagePath =
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/Apple-book.svg/800px-Apple-book.svg.png';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(children: [
+        SizedBox(
+          height: 100,
+          width: 300,
+          child: PngImage(name: ImageItems().appleBookWithoutPath),
+        ),
+        Image.network(
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/Apple-book.svg/800px-Apple-book.svg.png',
+          errorBuilder: (context, error, stackTrace) => const Icon(Icons.abc_outlined),
+        )
+      ]),
+    );
+  }
+}
+
+class ImageItems {
+  final String appleWithBook = "assets/apple-and-book-png-transparent-apple-and-book-images-274565.png";
+  final String appleBook = "assets/png/ic_apple_with_school.png";
+  final String appleBookWithoutPath = "ic_apple_with_school";
+}
+
+class PngImage extends StatelessWidget {
+  const PngImage({super.key, required this.name});
+  final String name;
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(_nameWithPath, fit: BoxFit.cover);
+  }
+
+  String get _nameWithPath => 'assets/png/$name.png';
+}
