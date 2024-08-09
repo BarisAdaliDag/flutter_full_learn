965c2d248c5858f8e6c28ef8328d88014a547311
barisAdaliDag <barisadali.dag@hotmail.com>
2024-08-09 09:52:07 +0300

    tabbar, model olusturma

---
+++ lib/202/a8_1_simpletabLearn.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SimpleTabLearn extends StatefulWidget {
+  const SimpleTabLearn({super.key});
+
+  @override
+  State<SimpleTabLearn> createState() => _SimpleTabLearnState();
+}
+
+class _SimpleTabLearnState extends State<SimpleTabLearn> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ import 'package:full_learn_flutter/101/a7_3_list_view_builder.dart';
 import 'package:full_learn_flutter/101/a7_5_navigation_learn.dart';
 import 'package:full_learn_flutter/101/text_learn_view.dart';
 import 'package:full_learn_flutter/101/a6_4_text_field_learn.dart';
+import 'package:full_learn_flutter/202/a8_1_tabLearn.dart';
 import 'package:full_learn_flutter/demos/a5_6_stack_demo_view.dart';
 import 'package:full_learn_flutter/demos/a7_1_color_demos_view.dart';
 import 'package:full_learn_flutter/demos/a7_1b_color_life_cycle.dart';
@@ -44,7 +45,7 @@ class MyApp extends StatelessWidget {
             elevation: 0),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
       ),
-      home: const NavigationLearn(),
+      home: const TabLearn(),
     );
   }
 }
